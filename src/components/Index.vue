750ca81ee65e0fc01d27af070fa17be841b2e0ea
banyq <banyq@yonyou.com>
2020-04-03 20:11:39 +0800

    re init

---
+++ src/components/Index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+
+    </div>
+</template>
+<script>
+    import HomeHeader from './home/HomeHeader';
+    export default {
+        name: 'index',
+        data() {
+            return {}
+        }
+    }
+
+</script>
+<style lang="less" scoped>
+    div {
+        h1 {
+            font-size: 2em;
+        }
+    }
+
+</style>
---
+++ src/components/home/HomeHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="home-header">
+
+    </div>
+</template>
+<script>
+    export default {
+        name: 'HomeHeader',
+        data() {
+            return {}
+        }
+    }
+
+</script>
+<style lang="less" scoped>
+.home-header {
+    background-color: @bg-color;
+}
+</style>
