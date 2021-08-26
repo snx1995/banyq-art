8a2588c94c585a325d4da2ab7328ff26ba24c141
banyq <snx1788@gmail.com>
2021-08-26 23:11:58 +0800

    test

--- src/component/index.vue
+++ src/component/index.vue
@@ -5,7 +5,7 @@
         </div>
 
         <v-snackbar v-model="showSnackbar" timeout="0">
-            Welcome!!(This is a test)
+            Welcome!!(Hello world)
 
             <template v-slot:action="{attrs}">
                 <v-btn v-bind="attrs" color="pink" text @click="showSnackbar = false">
